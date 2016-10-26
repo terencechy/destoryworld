f77e0d1f252ebd6335acbe7299f13560d862f2f3
Terence yungm <gccterence@gmail.com>
2016-10-26 17:16:51 +0800

    Initial Commit

---
+++ destorytheworld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  destorytheworld
+//
+//  Created by Yung Chi Hang on 26/10/16.
+//  Copyright © 2016 Yung Chi Hang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
