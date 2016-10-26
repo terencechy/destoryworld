535197602259f4eb0b8db9d1e2e446733eac165e
Terence yungm <gccterence@gmail.com>
2016-10-26 17:23:45 +0800

    hello world

--- destorytheworld/ViewController.swift
+++ destorytheworld/ViewController.swift
@@ -13,8 +13,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+   
+      print("helow world!!")
+    
     }
 
+    
+  
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
